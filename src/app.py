646394eae6cc371ec0077c0e0cbb9f24a5d70dc7
Violeta Ventura <violetaventura@gmail.com>
2020-05-13 20:45:19 +0000

    updated files

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
-import json, requests
+import json, requests, csv
+
 todos = []
 
 def get_todos():
-    global todos
     return todos
 
 def add_one_task(title):
@@ -10,27 +10,30 @@ def add_one_task(title):
     pass
 
 def print_list():
-    # your code here
-    pass
+    r = requests.get('https://assets.breatheco.de/apis/fake/todos/user/violetaventura001') 
+    r = r.json()
+    print(r)
 
 def delete_task(number_to_delete):
-    # your code here
-    pass
+    global todos
+    r = requests.get('https://assets.breatheco.de/apis/fake/todos/user/violetaventura001') 
+    todos.pop(int(number_to_delete))
+    d = requests.put('https://assets.breatheco.de/apis/fake/todos/user/violetaventura001', data = [])
+    print(todos) 
 
 def initialize_todos():
     global todos
-    r = requests.get('https://assets.breatheco.de/apis/fake/todos/user/alesanchezr') 
+    r = requests.get('https://assets.breatheco.de/apis/fake/todos/user/violetaventura001') 
     if(r.status_code == 404):
         print("No previous todos found, starting a new todolist")
-        r = requests.post(url = 'https://assets.breatheco.de/apis/fake/todos/user/alesanchezr', data = []) 
+        r = requests.post(url = 'https://assets.breatheco.de/apis/fake/todos/user/violetaventura001', data = []) 
         if r.status_code == 200:
             print("Tasks initialized successfully")
     else:
         print("A todo list was found, loading the todos...")
         todos = r.json()
-
     
-def save_todos():
+def save_todos(listToUpdate):
     # your code here
     pass
 
@@ -68,7 +71,7 @@ if __name__ == '__main__':
             add_one_task(title)
         elif response == "4":
             print("Saving todo's...")
-            save_todos()
+            save_todos(todos)
         elif response == "5":
             print("Loading todo's...")
             load_todos()
